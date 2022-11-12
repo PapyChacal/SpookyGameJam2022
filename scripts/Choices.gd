46b032d357a9f7acfe11c8819b07a9b809a5fe0d
Emilien Bauer <bauer.emilien@gmail.com>
2022-11-12 17:16:58 +0100

    Working Timer !!

--- scripts/Choices.gd
+++ scripts/Choices.gd
@@ -13,12 +13,24 @@ onready var texts : Array = [
 	$VBox/HBox/HBox/Choice3/VBox/Text,
 ]
 
+onready var npr_gauge = $VBox/HBoxTop/VBoxContainer/NPRGauge
+
+export(float) var answer_time = 7.0
+
+onready var timer : Timer = $Timer
+
 signal choice_made
 
 func _ready():
 	print(Interactions.lines["Dial1"])
 	print(Interactions.lines["Dial1"] == null)
 	set_description("Dial1")
+	
+	
+	timer.connect("timeout", self, "_on_say_nothing")
+
+func _process(delta):
+	npr_gauge.value = timer.time_left / answer_time
 
 func _on_choice(choice : int):
 	var d = GameState.le_dialogue
@@ -57,5 +69,9 @@ func set_description(id : String):
 		buttons[i].set_visible(true)
 	for i in range(nrep, 3):
 		buttons[i].set_visible(false)
+		
 	$VBox/HBoxTop/VBoxContainer.set_visible(is_question)
 	$VBox/HBox/HBox.set_visible(is_question)
+	
+	if is_question:
+		timer.start(answer_time)
