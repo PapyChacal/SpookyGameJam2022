903350225964743a28b0dbfabf2efaca7cb98fb5
Garip Windows <gkusoglu@etu.unistra.fr>
2022-11-13 14:45:56 +0100

    Add hall ellement

--- scripts/Locality/Hall1.gd
+++ scripts/Locality/Hall1.gd
@@ -4,6 +4,10 @@ extends Node2D
 func _ready():
 	var place = GameState.place_manager.places[name]
 	place.elements = {
-		'brother' : ''
+		'brother'      : '',
+		'staircase'    : 'goto:Room',
+		'toilet'       : 'Des5',
+		'plant'        : 'Des4',
+		'door_kitchen' : ''
 	}
 	place.inter = 'Dial1'
